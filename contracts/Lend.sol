a10d8b7616a988cb69f5982bc50ef630c6cf13e9
Andrew <aloveless@mail.csuchico.edu>
2018-04-22 16:35:13 -0700

    Update Lend.sol

--- contracts/Lend.sol
+++ contracts/Lend.sol
@@ -14,18 +14,22 @@ contract Lend is Ownable {
     ExternalStorage public externalStorage;
     TokenRegistry public tokenRegistry;
     PaymentHandler public paymentHandler;
-    address public tokenFeeContract; //Lend-X Token
+    address public tokenFeeContract;
+    // Major Releases/Stable Version - prior version deprecated
+    // Updates/New Functionality - prior version deprecated
+    // Bug/Security Fixes - prior version locked
     string constant public VERSION = "0.1.0";
     address public nextVersion = address(0); //linked list
     address public previousVersion; // linked list
     
-    //Storage Key Variables
-    bytes32 constant public LendeeKey = keccak256('lendee');
-    bytes32 constant public GuarantorKey = keccak256('guarantor');
-    
     bool public locked = false;
     bool public deprecated  = false;
     uint16 constant public EXTERNAL_QUERY_GAS_LIMIT = 4999;
+    
+    struct Debt {
+
+        bytes32 debtHash;
+    }
     
     function Lend(address _previousVersion, address _tokenFeeContract, address _storage, address _tokenRegistry, address _paymentHandler) public {
         externalStorage = ExternalStorage(_storage);
@@ -35,16 +39,30 @@ contract Lend is Ownable {
         previousVersion = _previousVersion;
     }
     
-    function requestUnsecuredDebt(address _tokenAddress, address _guarantor, uint256[9] memory _loanValues, bool[3] memory _loanOptions) public isDeprecated isLocked returns(bytes32){
-        require(validateLoanRequestArguments());
-        require(tokenRegistry.validateToken(_tokenAddress));
+    function submitDebtRequest(address[5] _loanAddresses, uint256[9] _loanValues, bool[3] _loanOptions) public isDeprecated isLocked returns(bytes32){
+        //require(validateLoanRequestArguments());
+        //require(tokenRegistry.validateToken(_tokenAddress));
         
-        uint256 requestCount = externalStorage.getRequestCount(msg.sender);
-        bytes32 requestID = keccak256(msg.sender, requestCount);
+        Debt memory debt = Debt({
+            maker: orderAddresses[0],
+            debtHash: getDebtHash(orderAddresses, orderValues)
+        });
+        
+        return = keccak256(
+            address(this),  //Lend Contract Address
+            _loanAddresses[1],  //Lendee
+            _loanAddresses[2],  //Lender
+            _loanAddresses[3],  //Principal Token
+            _loanAddresses[4],  //Payment Token
+            _loanValues[0],     //Principal Amount
+            _loanValues[1],     //Loan Start
+            _loanValues[2],     //Expires
+            _loanValues[3],     //Salt
+        );
         
         //Loan Addresses
-        externalStorage.setAddressValue(requestID, LendeeKey, msg.sender);
-        externalStorage.setAddressValue(requestID, GuarantorKey, _guarantor);
+        externalStorage.setAddressValue(requestID, keccak256('Principal'), msg.sender);
+        externalStorage.setAddressValue(requestID, keccak256('Guarantor'), _guarantor);
         //Loan Values
         externalStorage.setUIntValue(requestID, keccak256("Principal"), _loanValues[0]);
         externalStorage.setUIntValue(requestID, keccak256("LendingPeriodEnd"), _loanValues[1]);
@@ -52,7 +70,7 @@ contract Lend is Ownable {
         externalStorage.setUIntValue(requestID, keccak256("RepaymentDue"), _loanValues[3]);
         externalStorage.setUIntValue(requestID, keccak256("InterestFreePeriod"), _loanValues[4]);
         externalStorage.setUIntValue(requestID, keccak256("InterestRate"), _loanValues[5]);
-        externalStorage.setUIntValue(requestID, keccak256("InterestInterval"), _loanValues[6]);
+        externalStorage.setUIntValue(requestID, keccak256("PaymentInterval"), _loanValues[6]);
         externalStorage.setUIntValue(requestID, keccak256("FixedRepaymentAmount"), _loanValues[7]);
         externalStorage.setUIntValue(requestID, keccak256("LockUpPeriod"), _loanValues[8]);
         //Loan Options
@@ -61,15 +79,18 @@ contract Lend is Ownable {
         externalStorage.setBooleanValue(requestID, keccak256("Locked"), _loanOptions[2]);
         externalStorage.setBooleanValue(requestID, keccak256("Exists"), true);
         
+        //Lender Values
+        externalStorage.setLenderUIntStorage(requestID, _loanAddresses[2], keccak256("LenderPrincipal"), _loanValues[8]);
+        
         return requestID;
     }
     
-    function requestCollateralizedDebt() public returns(bool){
+    function submitCollateralizedDebtRequest() public returns(bool){
         
         return true;
     }
     
-    function requestCreditLine(bytes32 _messageHash) public returns(bytes32){
+    function offerCreditLine(bytes32 _messageHash) public returns(bytes32){
         
         return _messageHash;
     }
@@ -84,28 +105,25 @@ contract Lend is Ownable {
         return true;
     }
     
-    function getLendees() public returns(bool){
+    // function getLendees() public returns(bool){
         
-    }
+    // }
     
-    function getDebtRequestCount(address _requestor) public view returns(uint256){
-        return externalStorage.getRequestCount(_requestor);
-    }
+    // function getDebtRequestCount(address _requestor) public view returns(uint256){
+    //     return externalStorage.getRequestCount(_requestor);
+    // }
     
-    function getInterestRate(address _requestor) public view returns(uint256){
-        return externalStorage.getRequestCount(_requestor);
-    }
     function getInterestAccrued(address _requestor) public view returns(uint256){
         return externalStorage.getRequestCount(_requestor);
     }
     
-    function getRequests(address _storage, address _requestor) external view returns(bytes32[] memory loans){
-        uint256 requests = externalStorage.getRequestCount(_requestor);
-        for(uint256 i = 0; i < requests; i++){
-            loans[i] = keccak256(_requestor, i.add(1));
-        }
-        return loans;
-    }
+    // function getRequests(address _storage, address _requestor) external view returns(bytes32[] memory loans){
+    //     uint256 requests = externalStorage.getRequestCount(_requestor);
+    //     for(uint256 i = 0; i < requests; i++){
+    //         loans[i] = keccak256(_requestor, i.add(1));
+    //     }
+    //     return loans;
+    // }
     
     //only swaps out Lender address
     function transferDebtOwnership(bytes32 _requestID, uint256 _newOwner) public returns(bool){
