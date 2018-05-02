27029d3cb49701e4c83c2806ff0f5419c5f10d80
Andrew <aloveless@mail.csuchico.edu>
2018-05-01 23:42:48 -0700

    Update Ownable.sol

--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.23;
 
 /**
  * Ownable
@@ -10,7 +10,7 @@ pragma solidity ^0.4.18;
 contract Ownable {
     address public owner;
 
-    function Ownable() public {
+    constructor() public {
         owner = msg.sender;
     }
 
