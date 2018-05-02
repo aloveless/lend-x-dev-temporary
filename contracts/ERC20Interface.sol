aba2bb67614e38fc88e8a27adf76d6fec45ca0bf
Andrew <aloveless@mail.csuchico.edu>
2018-05-01 23:42:26 -0700

    Update ERC20Interface.sol

--- contracts/ERC20Interface.sol
+++ contracts/ERC20Interface.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.23;
 
 // Abstract contract for the full ERC20 Token standard
 // https://github.com/ethereum/EIPs/issues/20
@@ -6,10 +6,10 @@ pragma solidity ^0.4.18;
 contract ERC20Interface {
     
     function balanceOf(address _owner) public view returns (uint256);
-    function transfer(address _to, uint256 _value) public returns (bool success);
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
-    function approve(address _spender, uint256 _value) public returns (bool success);
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining);
+    function transfer(address _to, uint256 _value) public returns (bool);
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool);
+    function approve(address _spender, uint256 _value) public returns (bool);
+    function allowance(address _owner, address _spender) public view returns (uint256);
     function totalSupply() public view returns (uint256);
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value); 
